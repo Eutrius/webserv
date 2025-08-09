d0174231f860819b972b224708cf3c0a22e6227a
eutrius <eutrius@gmail.com>
2025-08-09 15:03:57 +0200

    feat(logging): add some basic logging

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -21,13 +21,12 @@ void Socket::init(t_host host, std::vector<Server> servers)
 	struct sockaddr_in address;
 	_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (_fd < 0)
-		throw std::runtime_error("Socket: failed to create socket");
+		throw std::runtime_error("Socket: failed to create socket.");
 
 	int opt = 1;
 	if (setsockopt(_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-		throw std::runtime_error("Socket: failed to set socket opt");
+		throw std::runtime_error("Socket: failed to set socket opt.");
 
-	std::cout << host.first << host.second << _fd << std::endl;
 	address.sin_family = AF_INET;
 	if (host.first == 0)
 		address.sin_addr.s_addr = INADDR_ANY;
@@ -37,17 +36,17 @@ void Socket::init(t_host host, std::vector<Server> servers)
 	std::memset(address.sin_zero, '\0', sizeof(address.sin_zero));
 
 	if (bind(_fd, (struct sockaddr *) &address, sizeof(address)) < 0)
-		throw std::runtime_error("Socket: failed to bind socket");
+		throw std::runtime_error("Socket: failed to bind socket.");
 
 	int flags = fcntl(_fd, F_GETFL, 0);
 	if (flags < 0)
-		throw std::runtime_error("Socket: failed to get socket flags");
+		throw std::runtime_error("Socket: failed to get socket flags.");
 
 	if (fcntl(_fd, F_SETFL, flags | O_NONBLOCK) < 0)
-		throw std::runtime_error("Socket: failed to make socket non-blocking");
+		throw std::runtime_error("Socket: failed to make socket non-blocking.");
 
 	if (listen(_fd, SOMAXCONN) < 0)
-		throw std::runtime_error("Socket: a socket failed to listen");
+		throw std::runtime_error("Socket: a socket failed to listen.");
 
 	_host = host;
 	_servers = servers;
